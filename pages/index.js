d30d9f92521228d5e80b3150dc8afa5dc4eab54a
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-01-02 17:00:30 -0500

    feature de particles.js con el error de deploy now.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Particles from 'react-particles-js';
 import '../src/index.css';
+import params from '../src/particlesjs-config.json';
 
-export default class Index extends React.Component {
-    componentDidMount() {
-      document.getElementsByTagName('canvas')[0].removeAttribute('height');
-      document.getElementsByTagName('canvas')[0].removeAttribute('width');
-      document.getElementsByTagName('canvas')[0].width = 0;
-      document.getElementsByTagName('canvas')[0].height = window.innerHeight + 20;
-      console.log(document.getElementsByTagName('canvas')[0].width = 0);
+function Index() {
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState();
+
+  // efecto side principal, sucede antes de cargar el componente.
+  // limpia el componente canvas de dos atributos que evitan el escalado correcto.
+  useEffect(() => {
+    setWidth(window.innerWidth + 20);
+    setHeight(window.innerHeight + 20);
+    document.getElementsByTagName('canvas')[0].removeAttribute('height');
+    document.getElementsByTagName('canvas')[0].removeAttribute('width');
+    document.getElementsByTagName('canvas')[0].width = window.innerWidth;
+    document.getElementsByTagName('canvas')[0].height = window.innerHeight;
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => { setWidth(window.innerWidth); setHeight(window.innerHeight); };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
     }
-  
-    render() {
-      return (
-        <div>
-             <Particles
-            height={'3000'}
-            style={{
-                backgroundColor: 'black' 
-            }}/>
-        </div>
-      )
-    } 
+  });
+
+  return (
+    <div>
+      <Particles
+        params={params}
+        style={{
+          backgroundColor: 'black' 
+        }}/>
+    </div>
+  )
 }
+
+export default Index;
