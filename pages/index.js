99c8e4db0a682af65d91a4bfa7ec78959096bc91
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-01-03 13:21:16 -0500

    particles 100% funcional, original y con estilos correctos

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,25 @@
 import React, { useEffect, useState } from "react";
-import Particles from 'react-particles-js';
-import '../src/index.css';
 import params from '../src/particlesjs-config.json';
+import '../src/index.css';
+import Content from "../src/content.js";
 
 function Index() {
-  const [width, setWidth] = useState();
-  const [height, setHeight] = useState();
+  var ParticlesJS = null;
 
-  // efecto side principal, sucede antes de cargar el componente.
-  // limpia el componente canvas de dos atributos que evitan el escalado correcto.
+  // importa dinámicamente en Client-side la librería particles
+  if (typeof window !== 'undefined') { 
+    ParticlesJS = require('particles.js');
+  }
+  // instancia las particulas ddinámicamente en Client-side
   useEffect(() => {
-    setWidth(window.innerWidth + 20);
-    setHeight(window.innerHeight + 20);
-    document.getElementsByTagName('canvas')[0].removeAttribute('height');
-    document.getElementsByTagName('canvas')[0].removeAttribute('width');
-    document.getElementsByTagName('canvas')[0].width = window.innerWidth;
-    document.getElementsByTagName('canvas')[0].height = window.innerHeight;
+    if (typeof window !== 'undefined') particlesJS('particles-js', params)
   }, []);
-
-  useEffect(() => {
-    const handleResize = () => { setWidth(window.innerWidth); setHeight(window.innerHeight); };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  });
 
   return (
     <div>
-      <Particles
-        params={params}
-        style={{
-          backgroundColor: 'black',
-          backgroundImage: 'radial-gradient(	#293336, #65737e)'
-        }}/>
+      {/* <img className={'background'} src={'../static/darkbg.jpg'} /> */}
+      <Content/>
+      {ParticlesJS !== null ? <div id="particles-js"></div> : "particles es nulo"}
     </div>
   )
 }
